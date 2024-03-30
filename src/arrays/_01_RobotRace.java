50e8a3857d6041099c3baf2403132aa16fdd12af
league <league@192.168.1.80>
2024-03-30 16:26:35 -0700

    a

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,23 +1,43 @@
 package arrays;
 
 import java.awt.AWTException;
-import java.awt.Robot;
 import java.util.Random;
+import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
+	private static int randomInt(){
+		Random ran =new Random();
+		int r=ran.nextInt(50);	
+		return r;
+	}
 	public static void main(String[] args) throws AWTException {
+
 	//2. create an array of 5 robots.
-		Robot[] rob=new Robot[5];
+		Robot[] rob=new Robot[8];
 	//3. use a for loop to initialize the robots.
 		for(int i=0;i<rob.length;i++) {
 			rob[i]=new Robot();
+			rob[i].setSpeed(10);
+			rob[i].moveTo(50+100*i, 500);
 		}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
-		
+		int k=0;
+
 	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
-		Random ran =new Random(50);
+	//   a random amount less than 50.\
+		boolean tOrF=true;
+		while(tOrF){
+			for(int i=0;i<=rob.length-1;) {
+				int r=randomInt();
+				rob[i].move(r);	
+				if(rob[i].getY()<0) {
+					System.out.println("Robot "+(i+1)+" won!");
+					System.exit(0);
+				}
+				i=i+1;
+			}
+		}
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 	//7. declare that robot the winner and throw it a party!
@@ -26,4 +46,5 @@ public class _01_RobotRace {
 
 	//9. make the robots race around a circular track.
 	}
+
 }
